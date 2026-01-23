c0ee30b34595dd56e66ae29ad8cea3c10c548b8d
Diego Valenzuela <valenzueladiegoe@gmail.com>
2026-01-22 23:50:10 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Preparado para futuras funciones (analytics, eventos, etc.)
+
+const accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(acc => {
+  acc.addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+});
